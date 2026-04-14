8f1b486544f8816014dd473523face7a0146c053
Nafis <nafisislam161@gmail.com>
2026-04-14 22:40:08 +0600

    link

--- components/customer/home/section-header.tsx
+++ components/customer/home/section-header.tsx
@@ -12,7 +12,7 @@ export function SectionHeader({ title, href }: Props) {
         {title}
       </h2>
       <Link 
-        href={href} 
+        href={`/category/${href}`} 
         className="text-[13px] font-bold text-[#b38716] transition-colors hover:text-slate-900"
       >
         আরও দেখুন »
